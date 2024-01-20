02b97a4c62a3d8c406df212d003a28cbc09e9b81
RexarSergey <DW9955@yandex.ru>
2024-01-20 15:40:48 +0300

    added task10

---
+++ Task10/Angle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    internal class Angle
+    {
+        double gradus_;
+        double min_;
+        double sec_;
+
+        public double Gradus
+        {
+            get { return gradus_; }
+            set 
+            { 
+                gradus_ = value; 
+            }
+        }
+
+        public double Min
+        {
+            get { return min_; }
+            set 
+            {
+                Gradus += (value / 60);
+                min_ = value % 60;
+            }
+        }
+
+        public double Sec
+        {
+            get { return sec_; }
+            set
+            {
+                Min += (value / 60);
+                sec_ = value % 60;
+            }
+        }
+
+        public Angle(double gradus, double min, double sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double ToRadans()
+        {
+            return (Math.PI / 180.0 * (Gradus + (Min / 60.0) + (Sec / 3600.0)));
+        }
+    }
+}
--- Task10/Program.cs
+++ Task10/Program.cs
@@ -10,6 +10,23 @@ namespace Task10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание про угол\n");
+
+            Console.WriteLine("Введите значение угла");
+            
+            Console.Write("Градусы: ");
+            double gradus = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Минут: ");
+            double min = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Секунды: ");
+            double sec = Convert.ToDouble(Console.ReadLine());
+
+            Angle angle = new Angle(gradus, min, sec);
+
+            Console.WriteLine("\nГрадусы в радианы: {0:F2}", angle.ToRadans());
+            Console.ReadKey();
         }
     }
 }
