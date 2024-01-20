18119787a315bb4c613b39d1b7af85cde1ef43fe
RexarSergey <DW9955@yandex.ru>
2024-01-20 15:19:39 +0300

    added task8 and task9

---
+++ Task8/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Task8
+{
+    internal class Program
+    {
+        static string path = @"D:\Work\VisualStudio2022\CSharpTasks\Task8\temp";
+
+        static void ReadFiles()
+        {
+            Console.WriteLine("Задача про чтение файлов\n");
+
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static void ReadDigits()
+        {
+            Console.WriteLine("Задача про цифры в файле\n");
+
+            string file_path = path + @"\nums.txt";
+            Random rand = new Random();
+
+            using (StreamWriter sw = new StreamWriter(file_path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rand.Next(-5, 11));
+                }
+            }
+
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(file_path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += Convert.ToInt16(sr.ReadLine());
+                }
+            }
+            Console.WriteLine("Сумма цифр равна: {0}", sum);
+        }
+
+        static void ReadText()
+        {
+            Console.WriteLine("Задача про текст в файле\n");
+
+            string file_path = path + @"\text.txt";
+
+            int symbol_num = 0;
+            int row_num = 0;
+            int word_num = 0;
+            using (StreamReader sr = new StreamReader(file_path))
+            {
+                while (true)
+                {
+                    string row = sr.ReadLine();
+                    if (row == null)
+                        break;
+
+                    row_num++;
+                    symbol_num += row.Length;
+                    word_num += row.Trim().Split().GetLength(0);
+                }
+            }
+
+            Console.WriteLine("Количество символов в тексте: {0}", symbol_num);
+            Console.WriteLine("Количество строк в тексте: {0}", row_num);
+            Console.WriteLine("Количество слов в тексте: {0}", word_num);
+        }
+
+        static void Main(string[] args)
+        {
+            ReadFiles();
+            Console.WriteLine(new string('-', 100));
+            ReadDigits();
+            Console.WriteLine(new string('-', 100));
+            ReadText();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second, action;
+
+            Console.WriteLine("Калькулятор на связи\n");
+
+            try
+            {
+                Console.Write("Введите первое целове число: ");
+                first = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.Write("Введите второе целове число: ");
+                second = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("\nВведите код операции:");
+                Console.WriteLine("\t1 - сложение");
+                Console.WriteLine("\t2 - вычитание");
+                Console.WriteLine("\t3 - произведение");
+                Console.WriteLine("\t4 - частное");
+
+                Console.Write("Код операции: ");
+                action = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (action)
+                {
+                    case 1:
+                        Console.WriteLine("{0} + {1} = {2}", first, second, (first + second));
+                        break;
+                    case 2:
+                        Console.WriteLine("{0} - {1} = {2}", first, second, (first - second));
+                        break;
+                    case 3:
+                        Console.WriteLine("{0} * {1} = {2}", first, second, (first * second));
+                        break;
+                    case 4:
+                        Console.WriteLine("{0} / {1} = {2}", first, second, (first / second));
+                        break;
+                    default:
+                        Console.WriteLine("Такой операции нет");
+                        break;
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка! Невернный формат вводных данных");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка! Попытка деления числа на 0");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
