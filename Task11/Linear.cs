d86d732a708859ab004e1525b7862eef381a38fe
RexarSergey <DW9955@yandex.ru>
2024-01-22 14:23:28 +0300

    added task11

---
+++ Task11/Linear.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    internal struct Linear
+    {
+        double k_;
+        double b_;
+
+        public Linear(double k, double b)
+        {
+            k_ = k;
+            b_ = b;
+        }
+
+        public string Root()
+        {
+            if (k_ == 0 && b_ == 0)
+            {
+                return "Бесконечное множество решений";
+            }
+            else if (k_ == 0)
+            {
+                return "Решений нет";
+            }
+            else if (b_ == 0)
+            {
+                return "x = 0";
+            }
+
+            return $"x = {-b_ / k_:F2}";
+        }
+    }
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание про линейное уравнение\n");
+
+            Console.WriteLine("Введите значение коэффицентов уравнения (0 = kx + b)");
+            Console.Write("k = ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear l = new Linear(k, b);
+            Console.WriteLine($"Решение: {l.Root()}");
+            Console.ReadKey();
+        }
+    }
+}
