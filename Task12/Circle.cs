5616e97aac2419bc401e1480e26ca4aaa2711d58
RexarSergey <DW9955@yandex.ru>
2024-01-22 14:55:46 +0300

    added task12

---
+++ Task12/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    internal static class Circle
+    {
+        public static double CircleLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        public static double CircleS(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        public static bool PointInCircle(double r, double x0, double y0, double x, double y)
+        {
+            return ( (x - x0) * (x - x0) + (y - y0) * (y - y0) ) <= (r * r);
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание про окружность\n");
+
+            Console.WriteLine("Введите параметры окружности");
+            Console.Write("Значение радиуса окружности: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Координаты центра окружности: ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Координаты точки: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($"Длина окружности: {Circle.CircleLength(r)}");
+            Console.WriteLine($"Площадь окружности: {Circle.CircleS(r)}");
+            Console.WriteLine($"Точка лежит в окружности: {Circle.PointInCircle(r, x0, y0, x, y)}");
+            Console.ReadKey();
+        }
+    }
+}
