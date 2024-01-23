b2de34d2ccc882b23644836f78c13bd43a0b1db5
RexarSergey <DW9955@yandex.ru>
2024-01-23 17:11:29 +0300

    added task15

---
+++ Task15/ISeries.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    internal interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    internal class ArithProgression : ISeries
+    {
+        int startVal_;
+        int step_;
+        int currVal_;
+
+        public int GetNext()
+        {
+            currVal_ += step_;
+            return currVal_;
+        }
+
+        public void Reset()
+        {
+            currVal_ = startVal_;
+        }
+
+        public void SetStep(int x)
+        {
+            step_ = x;
+        }
+
+        public void SetStart(int x)
+        {
+            startVal_ = x;
+            currVal_ = startVal_;
+        }
+    }
+
+    internal class GeomProgression : ISeries
+    {
+        int startVal_;
+        int step_;
+        int currVal_;
+
+        public int GetNext()
+        {
+            currVal_ *= step_;
+            return currVal_;
+        }
+
+        public void Reset()
+        {
+            currVal_ = startVal_;
+        }
+
+        public void SetStep(int x)
+        {
+            step_ = x;
+        }
+
+        public void SetStart(int x)
+        {
+            startVal_ = x;
+            currVal_ = startVal_;
+        }
+    }
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание про интерфейсы\n");
+            int firstVal = 3;
+            int step = 2;
+
+
+            Console.WriteLine("Арифметическая последовательность");
+            Console.WriteLine($"Начальное значение {firstVal} с шагом {step}");
+            
+            ArithProgression ap = new ArithProgression();
+            ap.SetStart(firstVal);
+            ap.SetStep(step);
+            Console.WriteLine($"{ap.GetNext()} {ap.GetNext()} {ap.GetNext()} {ap.GetNext()} {ap.GetNext()}");
+            ap.Reset();
+            Console.WriteLine($"{ap.GetNext()}\n");
+
+
+            Console.WriteLine("Геометрическая последовательность");
+            Console.WriteLine($"Начальное значение {firstVal} с шагом {step}");
+
+            GeomProgression gp = new GeomProgression();
+            gp.SetStart(firstVal);
+            gp.SetStep(step);
+            Console.WriteLine($"{gp.GetNext()} {gp.GetNext()} {gp.GetNext()} {gp.GetNext()} {gp.GetNext()}");
+            gp.Reset();
+            Console.WriteLine($"{gp.GetNext()}");
+
+            Console.ReadKey();
+        }
+    }
+}
