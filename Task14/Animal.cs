1887add3e61390d5b29357ac169d79f02b90b3b9
RexarSergey <DW9955@yandex.ru>
2024-01-23 16:41:16 +0300

    added task14

---
+++ Task14/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    internal abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name = "None")
+        {
+            Name = name;
+        }
+
+        public abstract string Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Say());
+        }
+    }
+
+    internal class Cat : Animal
+    {
+        private string name_;
+        public override string Name 
+        { 
+            get => name_; 
+            set => name_ = value; 
+        }
+
+        public Cat(string name)
+            :base(name) 
+        {}
+
+        public override string Say()
+        {
+            return "Мяу";
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        private string name_;
+        public override string Name
+        {
+            get => name_;
+            set => name_ = value;
+        }
+
+        public Dog(string name)
+            : base(name)
+        { }
+
+        public override string Say()
+        {
+            return "Гав";
+        }
+    }
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание про животных\n");
+
+            Cat cat = new Cat("Рыжик");
+            Dog dog = new Dog("Тузик");
+
+            cat.ShowInfo();
+            Console.WriteLine();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
